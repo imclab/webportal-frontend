8efa3014a0658eeab34a12ee751090511e71a996
eungg k <dfdgsdfg@gmail.com>
2013-04-27 13:26:12 +0900

    clean-up datatables-bootstrap

--- app/scripts/myitems.js
+++ app/scripts/myitems.js
@@ -29,8 +29,6 @@ define(['jquery', 'translate', 'datatables'], function ($, translate) {
 
         oTable: function() {
             return $('#example').dataTable({
-                'sDom': '<"row"<"span6"l><"span6"f>r>t<"row"<"span6"i><"span6"p>>',
-                'sPaginationType': 'bootstrap',
                 'bProcessing': true,
                 'bJQueryUI': true,
                 'bStateSave': false,
